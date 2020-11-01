54fb067280dbbd2a81eb2abe81a9ea4458ce0109
GrubeG <gggrube@uwalumni.com>
2020-10-31 21:03:42 -0500

    Adding Chloropleth colors

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+//First line of main.js...wrap everything in a self-executing anonymous function to move to local scope
+(function(){
+
+//pseudo-global variables
+var attrArray = ["GHG", "Waste", "TotalEner", "NonRenewEner", "WaterAbsAvg"];
+var expressed = attrArray[0]; //initial attribute
+    
 //begin script when window loads
 window.onload = setMap();
 
@@ -34,7 +41,64 @@ function setMap(){
         .await(callback);
     
     function callback(error, csvData, region, europe){
-        //create graticule generator
+        
+        //place graticule on the map
+        setGraticule(map, path);
+        
+        //translate europe TopoJSON
+        var regionalCountries = topojson.feature(region, region.objects.RegionalCountries),
+            europeCountries = topojson.feature(europe, europe.objects.EUCountries).features;
+        
+        //add regional countries to map
+        var regional = map.append("path")
+            .datum(regionalCountries)
+            .attr("class", "regional")
+            .attr("d", path);
+        
+        //join csv data to GeoJSON enumeration units
+        europeCountries = joinData(europeCountries, csvData);
+        
+        //create the color scale
+        var colorScale = makeColorScale(csvData);
+
+        //add enumeration units to the map
+        setEnumerationUnits(europeCountries, map, path, colorScale);
+        
+    };
+}; //end of setMap()
+    
+//function to create color scale generator
+function makeColorScale(data){
+    var colorClasses = [
+        "#D4B9DA",
+        "#C994C7",
+        "#DF65B0",
+        "#DD1C77",
+        "#980043"
+    ];
+
+    //create color scale generator
+    var colorScale = d3.scaleQuantile()
+        .range(colorClasses);
+
+    //create color scale generator
+    var colorScale = d3.scaleQuantile()
+        .range(colorClasses);
+
+    //build two-value array of minimum and maximum expressed attribute values
+    var minmax = [
+        d3.min(data, function(d) { return parseFloat(d[expressed]); }),
+        d3.max(data, function(d) { return parseFloat(d[expressed]); })
+    ];
+    
+    //assign two-value array as scale domain
+    colorScale.domain(minmax)
+
+    return colorScale;
+};
+    
+function setGraticule(map, path){
+    //create graticule generator
         var graticule = d3.geoGraticule()
             .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
         
@@ -51,12 +115,11 @@ function setMap(){
             .append("path") //append each element to the svg as a path element
             .attr("class", "gratLines") //assign class for styling
             .attr("d", path); //project graticule lines
-        
-        //translate europe TopoJSON
-        var regionalCountries = topojson.feature(region, region.objects.RegionalCountries),
-            europeCountries = topojson.feature(europe, europe.objects.EUCountries).features;
-        
-        //variables for data join
+};
+
+function joinData(europeCountries, csvData){
+    
+    //variables for data join
     var attrArray = ["GHG", "Waste", "TotalEner", "NonRenewEner", "WaterAbsAvg"];
 
     //loop through csv to assign each set of csv attribute values to geojson region
@@ -81,17 +144,13 @@ function setMap(){
             };
         };
     };
-        
-        
-        
-        
-        //add regional countries to map
-        var regional = map.append("path")
-            .datum(regionalCountries)
-            .attr("class", "regional")
-            .attr("d", path);
 
-        //add EU Countries to map
+    return europeCountries;
+};
+
+function setEnumerationUnits(europeCountries, map, path, colorScale){
+    
+    //add EU Countries to map
         var EuropeanUnionCountries = map.selectAll(".EuropeanUnionCountries")
             .data(europeCountries)
             .enter()
@@ -99,8 +158,22 @@ function setMap(){
             .attr("class", function(d){
                 return "EuropeanUnionCountries " + d.properties.GeogCode;
             })
-            .attr("d", path);
+            .attr("d", path)
+            .style("fill", function(d){
+                return choropleth(d.properties, colorScale);
+            });
+};
 
-        
+//function to test for data value and return color
+function choropleth(props, colorScale){
+    //make sure attribute value is a number
+    var val = parseFloat(props[expressed]);
+    //if attribute value exists, assign a color; otherwise assign gray
+    if (typeof val == 'number' && !isNaN(val)){
+        return colorScale(val);
+    } else {
+        return "#CCC";
     };
-}
+};
+    
+})(); //last line of main.js
